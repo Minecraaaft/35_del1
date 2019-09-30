86dfcc4a68f6a802169ae6f8e13fd1f24ace0503
Bendorff <andreasbendorff@hotmail.com>
2019-09-30 12:43:27 +0200

    Tilføjede Set DIce GUI

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import desktop_resources.GUI;
 import spil.Die;
 import spil.Player;
 
@@ -12,6 +13,9 @@ public class Test {
         Die d1 = new Die();
         d1.roll();
         System.out.println(d1.getFaceValue());
+
+
+        GUI.setDice(5,3);
 
     }
 }
