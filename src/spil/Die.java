b8e9e05e4eb595ffb9ba42791a3c3e09999c69d7
Max Thim Jeppesen <maxmagi@hotmail.com>
2019-09-30 12:04:06 +0200

    "Die klasse udfyldt"

--- src/spil/Die.java
+++ src/spil/Die.java
@@ -1,4 +1,23 @@
 package spil;
 
+import java.util.Random;
+
 public class Die {
+    //instans variabler/attributer
+    private int faceValue;
+
+    //konstruktor
+    public Die() {
+    }
+
+    //metoder
+    public int roll() {
+        Random random = new Random();
+        faceValue = random.nextInt(6) + 1;
+        return faceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
 }
