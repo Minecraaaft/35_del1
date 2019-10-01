e167116ce37b656d261b6ada92440643532c6813
Max Thim Jeppesen <maxmagi@hotmail.com>
2019-10-01 09:28:17 +0200

    "metoden Turn er flyttet til Player klassen og Game konstrukter er slettet"

--- src/spil/Game.java
+++ src/spil/Game.java
@@ -1,27 +1,6 @@
 package spil;
 
 public class Game {
-    //instans variabel
-
-    //konstruktør
-    public Game(){
-
-    }
-
-    //metoder
-    //public int checkPairs(){}
-
-
-    public static void turn(Player player){
-        int faceValueSum;
-
-        Die d1 = new Die();
-        Die d2 = new Die();
-        faceValueSum = d1.roll() + d2.roll();
-
-        System.out.println(faceValueSum);
-    }
-
 
     public static void main(String[] args) {
 
@@ -29,11 +8,11 @@ public class Game {
         Player player2 = new Player("Bob");
 
         do {
-            turn(player1);
+            player1.turn();
             if (player1.getPoints() >= 40)
                 break;
 
-            turn(player2);
+            player1.turn();
         } while (player1.getPoints() < 40 && player2.getPoints() < 40);
 
     }
